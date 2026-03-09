429428a9685c449af6bf4e3b947be9c5b855e220
Arcxs33 <157861628+Arcxs33@users.noreply.github.com>
2026-03-09 10:20:56 -0500

    Add intro lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Xiuwen Hu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone, my name is Xiuwen Hu. I am originally from Beijing, China, but I attend school in Pelham, Alabama as a junior now. In my free time, I participate in my school's theater and choir programs, and I am on the boys soccer team this season which has been incredibly fun. I love trying and enjoying new food, and my favorite type of cuisine is Japanese.")
 
 if __name__ == "__main__":
     main()
